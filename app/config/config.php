c847c6e327f25cc13d7efd0a2eefa7b22f96f228
Nova Triansyah Azis <36977706+NovaTri12@users.noreply.github.com>
2021-05-15 08:42:52 +0700

    add CONFIG DB

--- app/config/config.php
+++ app/config/config.php
@@ -1,11 +1,11 @@
 <?php
 
 
-define('BASEURL', 'http://localhost/taskbook/public');
+define('BASEURL', 'https://taskbooks.herokuapp.com/taskbook/public');
 
 // Database
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'taskbook');
+define('DB_HOST', 'us-cdbr-east-03.cleardb.com');
+define('DB_USER', 'b91cff22e9ec55');
+define('DB_PASSWORD', '53a06916');
+define('DB_NAME', 'heroku_c7249c0e75522ba');
